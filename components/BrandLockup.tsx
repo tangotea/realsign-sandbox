5744d91d10652a02f8c5578991d3632f0f883779
tangotea <44619962+tangotea@users.noreply.github.com>
2026-08-27 16:17:19 +0800

    Use trimmed logo and match header button height

--- components/BrandLockup.tsx
+++ components/BrandLockup.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function BrandLockup() {
   return (
     <div className="brand brand-lockup">
-      <Image src="/realsign-logo.png" width={52} height={52} alt="" priority />
+      <Image src="/realsign-logo.png" width={48} height={48} alt="" priority />
       <span className="brand-copy">
         <strong>REALSIGN</strong>
         <small>by RealSASL</small>
